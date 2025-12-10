2db326b72a1d95fc69fa7a46d0edc6fc767813f0
kk920106 <kk920106@naver.com>
2025-12-10 21:36:24 +0900

    불필요한 코드 제거

--- core-module/src/main/java/com/tastyhouse/core/repository/member/MemberJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tastyhouse.core.repository.member;
-
-import com.tastyhouse.core.entity.member.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberJpaRepository extends JpaRepository<Member, Long> {
-}
--- core-module/src/main/java/com/tastyhouse/core/repository/product/SupplyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tastyhouse.core.repository.product;
-
-import com.tastyhouse.core.entity.product.Supply;
-
-import java.util.List;
-
-public interface SupplyRepository {
-    List<Supply> findAll();
-}
