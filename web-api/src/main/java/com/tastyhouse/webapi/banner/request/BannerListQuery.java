3c060872a6ddf11cd0932aa610a3bcb50d7a9b94
kk920106 <kk920106@naver.com>
2026-03-12 22:23:53 +0900

    refactor(api): getter 메서드를 record 구성 요소로 대체

--- web-api/src/main/java/com/tastyhouse/webapi/banner/request/BannerListQuery.java
+++ web-api/src/main/java/com/tastyhouse/webapi/banner/request/BannerListQuery.java
@@ -1,9 +1,7 @@
 package com.tastyhouse.webapi.banner.request;
 
-import lombok.Data;
-
-@Data
-public class BannerListQuery {
-    private String title;
-    private Boolean active;
+public record BannerListQuery(
+        String title,
+        Boolean active
+) {
 }
